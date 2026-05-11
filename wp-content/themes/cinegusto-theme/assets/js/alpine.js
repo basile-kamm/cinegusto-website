bf290b1685f4ad05b756aabc0d706cc7bcbb4ea2
Basile Kamm <hello@basile-kamm.ch>
2026-05-11 11:59:50 +0200

    X-collapse install

--- wp-content/themes/cinegusto-theme/assets/js/alpine.js
+++ wp-content/themes/cinegusto-theme/assets/js/alpine.js
@@ -1,8 +1,10 @@
 import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
 
 const alpineModule = () => {
   window.Alpine = Alpine;
 
+  Alpine.plugin(collapse);
   Alpine.start();
 };
 

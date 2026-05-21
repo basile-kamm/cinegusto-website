0aa580b7123dc2f3dce30a71f4430ef220a21a66
Basile Kamm <hello@basile-kamm.ch>
2026-05-21 09:51:29 +0200

    VOD anim & iframe load on click

--- wp-content/themes/cinegusto-theme/assets/js/gsap.js
+++ wp-content/themes/cinegusto-theme/assets/js/gsap.js
@@ -5,6 +5,7 @@ import {ScrollTrigger} from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
+  //Header animation
   let lastScrollPosition = 0;
 
   window.addEventListener("scroll", () => {
@@ -24,4 +25,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     lastScrollPosition = currentScrollPosition;
   });
+
+  // Canap VOD animation
+  gsap.fromTo(
+    ".canap",
+    {
+      top: "100%",
+    },
+    {
+      top: "20%",
+      left: "100%",
+      rotate: 420,
+      scale: 0.6,
+      scrollTrigger: {
+        trigger: ".canap-container",
+        start: "top 60%",
+        end: "top -50%",
+        markers: true,
+        scrub: true,
+      },
+    },
+  );
+  if (window.innerWidth > 768) {
+    gsap.fromTo(
+      ".canap",
+      {
+        top: "100%",
+      },
+      {
+        top: "-20%",
+        left: "100%",
+        rotate: 420,
+        scale: 0.6,
+        scrollTrigger: {
+          trigger: ".canap-container",
+          start: "top 60%",
+          end: "top -50%",
+          markers: true,
+          scrub: true,
+        },
+      },
+    );
+  }
 });
